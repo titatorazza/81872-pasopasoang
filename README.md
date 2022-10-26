f2ac1e903ab00cca09225fdbdee0cf49d350d163
titatorazza <86207334+titatorazza@users.noreply.github.com>
2022-10-26 08:36:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 81872-pasopasoang
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/81872-pasopasoang)
---
+++ src/app/services/mock-articulos-familias.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { ArticulosFamilias } from '../models/articulo-familia';
+@Injectable({
+  providedIn: 'root',
+})
+export class MockArticulosFamiliasService {
+  constructor() {}
+  get() {
+    return of(ArticulosFamilias);
+  }
+}
